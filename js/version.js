38f16ec9d9d3c59fc8733c30745b621c476303af
tomo <heineko@gmail.com>
2025-09-24 23:49:17 +0900

    first up bikkuri

---
+++ js/version.js
@@ -0,0 +1,4 @@
+// Shared application version for cache-busting and display
+// Update this value on deploys.
+window.__APP_VERSION__ = '2025-09-23-02';
+window.APP_VERSION = window.__APP_VERSION__;

101257d7d63596a61601cba67b2c0d77fb8239a2
tomo <heineko@gmail.com>
2025-10-09 23:59:49 +0900

    1009 update

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-10-08-04';
+window.__APP_VERSION__ = '2025-10-09-01';
 window.APP_VERSION = window.__APP_VERSION__;

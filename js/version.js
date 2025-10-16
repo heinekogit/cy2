6c2507d70e8d36a85289d8998134905160cd945b
tomo <heineko@gmail.com>
2025-10-17 00:35:28 +0900

    run login error3

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-10-16-03';
+window.__APP_VERSION__ = '2025-10-16-04';
 window.APP_VERSION = window.__APP_VERSION__;

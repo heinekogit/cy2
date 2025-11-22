6446c1a86ffc65894a83448d71f035e403bdb8f0
tomo <heineko@gmail.com>
2025-11-22 21:16:01 +0900

    描画保存ボケ直し

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-11-22-13';
+window.__APP_VERSION__ = '2025-11-22-14';
 window.APP_VERSION = window.__APP_VERSION__;

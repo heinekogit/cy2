2598a5b41cfd56f18c564f6f6937fa76f95404a0
tomo <heineko@gmail.com>
2025-12-03 00:01:41 +0900

    写真サムネ表示01

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-11-27-01';
+window.__APP_VERSION__ = '2025-12-01-01';
 window.APP_VERSION = window.__APP_VERSION__;

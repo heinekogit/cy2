bdf97fe77a2e06a80aa9946bb896196369afcb5c
tomo <heineko@gmail.com>
2025-12-07 23:29:43 +0900

    記録開始ボタン不具合修正

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-03-01';
+window.__APP_VERSION__ = '2025-12-07-01';
 window.APP_VERSION = window.__APP_VERSION__;

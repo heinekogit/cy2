47ff1c754819db5514fec4f69e2ba2dfff5fa81f
tomo <heineko@gmail.com>
2025-12-11 23:04:42 +0900

    ログイン等不整合06GPT

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-10-05-runfix4';
+window.__APP_VERSION__ = '2025-12-11-01-runfix5';
 window.APP_VERSION = window.__APP_VERSION__;

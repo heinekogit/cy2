d6bbaf1c47208f235c5f12c22662051cd36da6c2
tomo <heineko@gmail.com>
2025-12-11 22:28:49 +0900

    ログイン等不整合04

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-10-03-runfix2';
+window.__APP_VERSION__ = '2025-12-10-04-runfix3';
 window.APP_VERSION = window.__APP_VERSION__;

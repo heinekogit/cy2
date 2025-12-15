9a3ca65abdb6d23ae3b5612a0fecb50b7d6dbdea
tomo <heineko@gmail.com>
2025-12-15 22:21:18 +0900

    コード表記の統一

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-11-03-runfix7';
+window.__APP_VERSION__ = '2025-12-15-01-runfix7';
 window.APP_VERSION = window.__APP_VERSION__;

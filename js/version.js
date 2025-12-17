2a553d109b69ab4a86400b9e5562467d53aa7405
tomo <heineko@gmail.com>
2025-12-17 22:30:52 +0900

    run.html不調GTP参

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-17-06-photo-debug-geo';
+window.__APP_VERSION__ = '2025-12-17-08-startbtn-GPT';
 window.APP_VERSION = window.__APP_VERSION__;

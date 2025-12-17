cc3af6240c28ee3089f613cec8ffda55bb9f04ac
tomo <heineko@gmail.com>
2025-12-17 22:00:35 +0900

    run.html不具合

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-17-03-photo-buttons';
+window.__APP_VERSION__ = '2025-12-17-05-photo-buttons-hidebadge';
 window.APP_VERSION = window.__APP_VERSION__;

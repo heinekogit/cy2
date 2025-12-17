067bf7f3372024af91340e58a688130acc38335c
tomo <heineko@gmail.com>
2025-12-17 23:52:38 +0900

    run.html認証失敗

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-17-10-startbtn-clean_GPT03';
+window.__APP_VERSION__ = '2025-12-17-11-auth-timeout';
 window.APP_VERSION = window.__APP_VERSION__;

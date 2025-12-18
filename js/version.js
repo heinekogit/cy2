d9d707d6568d67bf659d5d8ff78f69878263340b
tomo <heineko@gmail.com>
2025-12-18 17:03:47 +0900

    キャプチャ送り

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-12_iikagennisite';
+window.__APP_VERSION__ = '2025-12-18-12_capcture';
 window.APP_VERSION = window.__APP_VERSION__;

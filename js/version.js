a42c2d3e0cfc585523ffcd3c8b76fb2ef386b115
tomo <heineko@gmail.com>
2025-12-18 09:01:29 +0900

    run.html認証失敗続き11

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-17-12-auth-order';
+window.__APP_VERSION__ = '2025-12-18-01';
 window.APP_VERSION = window.__APP_VERSION__;

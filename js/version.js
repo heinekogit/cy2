6ebdd2614ffa61fc7a444492a24e4a5ba6d7611a
tomo <heineko@gmail.com>
2025-12-18 16:56:29 +0900

    いい加減にして

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-12_dondakeeee';
+window.__APP_VERSION__ = '2025-12-18-12_iikagennisite';
 window.APP_VERSION = window.__APP_VERSION__;

5835a4b15449a5c2ca951d88d1dfbbbb074a1cf6
tomo <heineko@gmail.com>
2025-12-18 10:45:09 +0900

    run.html_GPT_再codex

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-06_GPT_hand_03';
+window.__APP_VERSION__ = '2025-12-18-06_GPT_codex';
 window.APP_VERSION = window.__APP_VERSION__;

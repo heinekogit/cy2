71afea9a72abd91c5cfb14624a33ae03b03ca8ae
tomo <heineko@gmail.com>
2025-12-18 09:54:33 +0900

    run.html_GPTと手動

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-04_GPT';
+window.__APP_VERSION__ = '2025-12-18-05_GPT_hand';
 window.APP_VERSION = window.__APP_VERSION__;

da357c27220497f0f288e77e551912e01aaba798
tomo <heineko@gmail.com>
2025-12-18 12:16:25 +0900

    codexによるJSテスト03

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-11_testJS_codex02';
+window.__APP_VERSION__ = '2025-12-18-11_testJS_codex03';
 window.APP_VERSION = window.__APP_VERSION__;

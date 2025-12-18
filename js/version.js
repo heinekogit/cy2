d8b69b1efe7ca77069166355abaf6cec1c92e8e9
tomo <heineko@gmail.com>
2025-12-18 11:10:25 +0900

    run.html_バグループ

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-07_codex';
+window.__APP_VERSION__ = '2025-12-18-07_bugloop';
 window.APP_VERSION = window.__APP_VERSION__;

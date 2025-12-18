2458169b4509e92cca414a4b40ce06e90a3c98d9
tomo <heineko@gmail.com>
2025-12-18 11:28:35 +0900

    test02

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-07_test';
+window.__APP_VERSION__ = '2025-12-18-11_test02';
 window.APP_VERSION = window.__APP_VERSION__;

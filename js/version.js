6de470058b013ca01331f663540bee6104ef47ff
tomo <heineko@gmail.com>
2025-12-18 13:52:15 +0900

    runのsupabase修正02

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-11_supa_revise';
+window.__APP_VERSION__ = '2025-12-18-11_supa_revise02';
 window.APP_VERSION = window.__APP_VERSION__;

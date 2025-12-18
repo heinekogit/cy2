4d8b41c2b92f79ad72598871795413e7b6d8a4c6
tomo <heineko@gmail.com>
2025-12-18 10:55:59 +0900

    run.html_codexで修正

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-18-06_GPT_codex';
+window.__APP_VERSION__ = '2025-12-18-07_codex';
 window.APP_VERSION = window.__APP_VERSION__;

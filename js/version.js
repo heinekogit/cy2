8e20ce840b5dbe3a2a7f56ebeb51fe5354ef3779
tomo <heineko@gmail.com>
2025-12-23 13:39:52 +0900

    latlng_テスト後

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '2025-12-21-01_latlng_visible_02';
+window.__APP_VERSION__ = '2025-12-23-01_latlng_visible_03';
 window.APP_VERSION = window.__APP_VERSION__;

c5066b2f01620a5e11b711ceffd91ed94c234b48
tomo <heineko@gmail.com>
2026-01-13 09:37:31 +0900

    edit page plus_02

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '20260112-01';
+window.__APP_VERSION__ = '20260113-01';
 window.APP_VERSION = window.__APP_VERSION__;

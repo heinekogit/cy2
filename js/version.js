c30404571432929ed127d5c25983dbf1d64632e8
tomo <heineko@gmail.com>
2026-06-30 23:42:18 +0900

    マイルート一覧表示不具合修正

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Shared application version for cache-busting and display
 // Update this value on deploys.
-window.__APP_VERSION__ = '20260627-02';
+window.__APP_VERSION__ = '20260630-01';
 window.APP_VERSION = window.__APP_VERSION__;
